bac1bb2c899e6ff46955f03f446d29268a8869f1
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-10 19:15:31 -0300

    Project restructuring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import darkTheme from './themeConfig'
 import Home from './views/Home'
 
-function App() {
-  return <Home />
+const App = () => {
+  return (
+    <Fragment>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Home />
+      </ThemeProvider>
+    </Fragment>
+  )
 }
 
 export default App
