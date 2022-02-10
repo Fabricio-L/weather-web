b6ca414c8866b9121984305a74e31084ee123cf5
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-10 19:26:51 -0300

    Files and folders created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Home from "./views/Home"
+import Home from './views/Home'
+import Header from './layouts/header/Header'
+import Footer from './layouts/footer/Footer'
+import { Fragment } from 'react'
 
 function App() {
-  return <Home />
+  return (
+    <Fragment>
+      <Header />
+      <Home />
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default App
