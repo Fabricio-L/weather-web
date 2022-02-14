105fdec9e127a34bb7b3982e29350a7f68947f13
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-14 06:03:28 -0300

    List of places added

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,96 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Paper, Typography, Box } from '@mui/material'
 import GoogleMapReact from 'google-map-react'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined'
+import createMapOptions from './createMapOptions'
 import useStyles from './styles'
 
 const Map = () => {
   const classes = useStyles()
-  const coordinates = { lat: -37.339284, lng: -81.361661 }
+  const [coordinates, setCoordinates] = useState({
+    lat: -37.339284,
+    lng: -81.361661,
+  })
+  const [indexPlace, setIndexPlace] = useState(0)
 
-  const createMapOptions = () => {
-    return {
-      styles: [
-        { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-        { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-        { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#d59563' }],
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#d59563' }],
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{ color: '#263c3f' }],
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#6b9a76' }],
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{ color: '#38414e' }],
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{ color: '#212a37' }],
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#9ca5b3' }],
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{ color: '#746855' }],
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{ color: '#1f2835' }],
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#f3d19c' }],
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{ color: '#2f3948' }],
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#d59563' }],
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{ color: '#17263c' }],
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#515c6d' }],
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{ color: '#17263c' }],
-        },
-      ],
-    }
+  const listPlaces = [
+    {
+      lat: '-27.4806',
+      lng: '-58.8341',
+    },
+    {
+      lat: '-34.6132',
+      lng: '-58.3772',
+    },
+    {
+      lat: '-34.8335',
+      lng: '-56.1674',
+    },
+    {
+      lat: '-23.547',
+      lng: '-46.6361',
+    },
+    {
+      lat: '-12.0432',
+      lng: '-77.0282',
+    },
+  ]
+
+  const handleCoordinates = (e) => {
+    setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+  }
+
+  const handleSelectPlace = (e) => {
+    setIndexPlace(e)
   }
 
   return (
@@ -102,9 +53,15 @@ const Map = () => {
         defaultZoom={14}
         zoom={4}
         options={createMapOptions}
-        // onChange={}
-        // onChildClick={}
-      ></GoogleMapReact>
+        onChange={handleCoordinates}
+        onChildClick={handleSelectPlace}
+      >
+        {listPlaces.map((place, index) => (
+          <div lat={place.lat} lng={place.lng} key={index}>
+            <LocationOnOutlinedIcon />
+          </div>
+        ))}
+      </GoogleMapReact>
     </Box>
   )
 }
