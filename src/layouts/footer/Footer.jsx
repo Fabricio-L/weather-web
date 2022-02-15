ffb057a2536f789b1015a97e08ca86e3eb6af917
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-15 01:56:16 -0300

    Fix build erros

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -13,11 +13,12 @@ const Footer = () => {
     <Box display={'flex'} className={classes.root}>
       <Box flexGrow={1} className={classes.root} style={{ marginLeft: '10px' }}>
         <Typography variant="h7">Technologies</Typography>
-        <img src={reactjs} className={classes.imgFooter} />
-        <img src={mui} className={classes.imgFooter} />
+        <img src={reactjs} className={classes.imgFooter} alt="React JS" />
+        <img src={mui} className={classes.imgFooter} alt="Material UI" />
         <img
           src={weather}
           style={{ width: '90px', height: 'fit-content', margin: '10px' }}
+          alt="Open Weather"
         />
       </Box>
 
@@ -25,11 +26,13 @@ const Footer = () => {
       <a
         href="https://github.com/Fabricio-L/weather-web/tree/master"
         target="_blank"
+        rel="noreferrer"
       >
         <img
           src={github}
           className={classes.imgFooter}
           style={{ marginRight: '10px' }}
+          alt="Github"
         />
       </a>
     </Box>
