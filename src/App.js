d663eafaf337746a86bbe3570dd3979943b48b6a
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-15 00:26:19 -0300

    Context function to select places

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles'
 import { Box } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 import darkTheme from './themeConfig'
+import CityProvider from './context/CityContext'
 import Home from './views/Home'
 import Header from './layouts/header/Header'
 import Footer from './layouts/footer/Footer'
@@ -10,14 +11,16 @@ import Footer from './layouts/footer/Footer'
 const App = () => {
   return (
     <Fragment>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Box height={'100vh'}>
-          <Header />
-          <Home />
-          <Footer />
-        </Box>
-      </ThemeProvider>
+      <CityProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Box height={'100vh'}>
+            <Header />
+            <Home />
+            <Footer />
+          </Box>
+        </ThemeProvider>
+      </CityProvider>
     </Fragment>
   )
 }
--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,13 +1,44 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useContext, useState } from 'react'
 import { Box, FormControl, Select, InputLabel, MenuItem } from '@mui/material'
+import { CityContext } from '../../context/CityContext'
 import useStyles from './styles'
 
 const Card = () => {
   const classes = useStyles()
-  const [city, setCity] = useState('')
+  const context = useContext(CityContext)
+  const [city, setCity] = useState('Corrientes,AR')
+
+  const cities = [
+    {
+      name: 'Corrientes,AR',
+      lat: '-27.4806',
+      lng: '-58.8341',
+    },
+    {
+      name: 'Buenos Aires,AR',
+      lat: '-34.6132',
+      lng: '-58.3772',
+    },
+    {
+      name: 'Montevideo,UY',
+      lat: '-34.8335',
+      lng: '-56.1674',
+    },
+    {
+      name: 'São Paulo,BR',
+      lat: '-23.547',
+      lng: '-46.6361',
+    },
+    {
+      name: 'Lima,PE',
+      lat: '-12.0432',
+      lng: '-77.0282',
+    },
+  ]
 
   const handleChange = (event) => {
     setCity(event.target.value)
+    context.setCity(cities.find((x) => x.name === event.target.value))
   }
 
   return (
@@ -15,12 +46,12 @@ const Card = () => {
       <Box className={classes.root}>
         <Box className={classes.cardContainer}>
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">City</InputLabel>
+            <InputLabel id="Label">City</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId="City-Select"
+              id="Select"
               value={city}
-              label="Age"
+              label="City"
               onChange={handleChange}
             >
               <MenuItem value={'Corrientes,AR'}>Corrientes</MenuItem>
